515cb07694cff05228f9c7f033d24beec5356c25
tmdrhsl4857 <tmdrhsl4857@gmail.com>
2025-01-14 15:11:44 +0900

    Update

--- start.js
+++ start.js
@@ -1,2 +1,2 @@
-const response = fetch("<https://my-json-server.typicode.com/tmdrhsl4857/typicode_json>");
+const response = fetch("<http://my-json-server.typicode.com/tmdrhsl4857/typicode_json>");
 console.log(response);
