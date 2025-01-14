1f8bef13e476667cfddc4d18f2904d4ca89483a9
tmdrhsl4857 <tmdrhsl4857@gmail.com>
2025-01-14 18:08:25 +0900

    Make server base

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, Node.js!");
+const express = require('express');
+const start = express();
+const port = 3000;
+
+start.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+start.get('/example', (req, res, next) => {
+    console.log("첫 번째 콜백 함수");
+    next();
+}, (req, res) => {
+    res.send('두 번째 콜백 함수');
+});
+
+start.listen(port, () => {
+    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+});
