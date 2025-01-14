f79b0c507e6d29d693830f467c9262cc7ba0e36a
tmdrhsl4857 <tmdrhsl4857@gmail.com>
2025-01-15 01:31:31 +0900

    d

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs'); // 파일 시스템 모듈
+const path = require('path'); // 파일 경로 조작 모듈
+
 const app = express();
-const port = 3000;
+const port = 8848;
+const filePath = path.join(__dirname, 'db.json');
+
+var jsonData;
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    jsonData = JSON.parse(data);
+    console.log(jsonData);
+});
 
 app.use(express.static('public'));
 
@@ -10,33 +19,13 @@ app.get('/', (req, res) => {
     res.sendfile(__dirname + "/" + "index.html");
 })
 
-app.get('/bday', (req, res) => {
-    const number = parseInt(req.query.number, 10); // 쿼리 스트링에서 number 값 가져오기
-  
-    if (isNaN(number)) {
-      return res.send('번호를 입력해주세요.');
-    }
-  
-    const filePath = path.join(__dirname, 'db.json');
-  
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error('파일 읽기 에러:', err);
-        return res.status(500).send('서버 에러');
-      }
-  
-      const jsonData = JSON.parse(data);
-      const character = jsonData.character.find((item) => item.id === number);
-  
-      if (character) {
-        // 이름과 생년월일 반환
-        res.send(`${character.name}의 생일은 ${character.month}월 ${character.day}일입니다.`);
-      } else {
-        // 해당 번호가 없는 경우
-        res.send('해당 번호의 데이터를 찾을 수 없습니다.');
-      }
-    });
-  });
+app.get('/getbday/:Id', (req, res) => {
+    const uid = req.params.Id
+    var snad = jsonData.chara.find(i => i.id === uid);
+    res.send(snad);
+    res.send(JSON.stringify(req));
+    console.log(snad);
+})
 
 app.listen(port, () => {
     console.log(`서버가 실행됩니다. http://localhost:${port}`);
